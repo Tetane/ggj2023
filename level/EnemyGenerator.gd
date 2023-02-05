311bec1f040c2e6bb4d4c37a505cf648acde63a5
Tetane <stephane.roussel.fr@gmail.com>
2023-02-05 11:44:40 +0100

    add ressource generator

--- level/EnemyGenerator.gd
+++ level/EnemyGenerator.gd
@@ -1,16 +1,27 @@
 extends Path2D
 
 var rng = RandomNumberGenerator.new()
+var rng2 = RandomNumberGenerator.new()
+var rng3 = RandomNumberGenerator.new()
+var rng4 = RandomNumberGenerator.new()
 onready var enemy = preload("res://enemys/enemy1/enemy1.tscn")
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
+onready var ressourceArray = [
+	preload("res://resources/earth/earth.tscn"),
+	preload("res://resources/water/water.tscn"),
+	preload("res://resources/light/light.tscn")
+]
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$RessourcesTimer.connect("timeout", self, "generate_ressource")
 	rng.randomize()
-	pass # Replace with function body.
+	rng2.randomize()
+	rng3.randomize()
+	rng4.randomize()
+
 
 func generate_enemy():
 	$PathFollow2D.unit_offset = rng.randf_range(0.0, 1.0)
@@ -23,8 +34,13 @@ func generate_enemy():
 	print("add_child")
 	#owner.add_child(newenemy)
 	
-#func _process(delta):
-#	pass
+
+func generate_ressource():
+	if get_node("%Tree").move_direction != Vector2(0,0):
+		$PathFollow2D.unit_offset = rng2.randf_range(0.0, 1.0)
+		var newressource = ressourceArray[rng3.randi_range(0,2)].instance()
+		newressource.position = $PathFollow2D.global_position
+		get_tree().root.add_child(newressource)
 
 
 func _on_Timer_timeout():
